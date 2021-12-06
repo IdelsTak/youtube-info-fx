05eaaf6744434fd2cd8fdc4755ff56bb4f6b84cb
Hiram Kamau <hiram.kamau@outlook.com>
2021-12-06 19:14:18 +0300

    Create VideoCategory.java

---
+++ YouTubeInfoFX/src/main/java/com/github/idelstak/youtubeinfofx/spi/VideoCategory.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2021
+ */
+package com.github.idelstak.youtubeinfofx.spi;
+
+/**
+ *
+ * @author Hiram K. <https://github.com/IdelsTak>
+ */
+public interface VideoCategory {
+
+  String getId();
+
+  String getTitle();
+
+}
