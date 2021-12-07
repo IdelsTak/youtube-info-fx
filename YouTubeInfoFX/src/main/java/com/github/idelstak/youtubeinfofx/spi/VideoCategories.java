c0fe0212cae69fa15a2f5473bb7ffc8d432fa655
Hiram Kamau <hiram.kamau@outlook.com>
2021-12-07 10:18:49 +0300

    Update VideoCategories.java

--- YouTubeInfoFX/src/main/java/com/github/idelstak/youtubeinfofx/spi/VideoCategories.java
+++ YouTubeInfoFX/src/main/java/com/github/idelstak/youtubeinfofx/spi/VideoCategories.java
@@ -6,6 +6,8 @@ package com.github.idelstak.youtubeinfofx.spi;
 import com.github.idelstak.youtubeinfofx.api.YouTubeVideoCategory;
 import com.google.api.services.youtube.YouTube;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
+import com.github.idelstak.youtubeinfofx.api.service.GoogleService;
 
 /**
  *
@@ -13,16 +15,24 @@ import java.io.IOException;
  */
 public abstract class VideoCategories {
 
-  private final YouTube youTube;
+  private final GoogleService gs;
 
-  protected VideoCategories(YouTube youTube) {
-    this.youTube = youTube;
+  protected VideoCategories(GoogleService gs) {
+    this.gs = gs;
   }
 
   public abstract Iterable<VideoCategory> getVideoCategories() throws IOException;
 
+  protected String getKey() {
+    return gs.getKey();
+  }
+
   protected YouTube getService() {
-    return youTube;
+    try {
+      return gs.getService();
+    } catch (GeneralSecurityException | IOException ex) {
+      throw new RuntimeException(ex);
+    }
   }
 
   public VideoCategory toVideoCategory(com.google.api.services.youtube.model.VideoCategory category) {
