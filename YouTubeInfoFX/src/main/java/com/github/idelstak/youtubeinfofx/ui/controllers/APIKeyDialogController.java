41790b1d607ee63b36b064cf7ba2def9e2572eef
Hiram Kamau <hiram.kamau@outlook.com>
2021-12-07 11:03:48 +0300

    Create APIKeyDialogController.java

---
+++ YouTubeInfoFX/src/main/java/com/github/idelstak/youtubeinfofx/ui/controllers/APIKeyDialogController.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2021
+ */
+package com.github.idelstak.youtubeinfofx.ui.controllers;
+
+import javafx.fxml.FXML;
+
+/**
+ *
+ * @author Hiram K. <https://github.com/IdelsTak>
+ */
+public class APIKeyDialogController {
+
+  @FXML
+  void initialize() {
+
+  }
+}
