a54fa85fe4a44dbd6e8d866bb279f76f753d2a43
Hiram Kamau <hiram.kamau@outlook.com>
2021-12-07 11:50:12 +0300

    Update YouTubeInfoFXApp.java

--- YouTubeInfoFX/src/main/java/com/github/idelstak/youtubeinfofx/ui/YouTubeInfoFXApp.java
+++ YouTubeInfoFX/src/main/java/com/github/idelstak/youtubeinfofx/ui/YouTubeInfoFXApp.java
@@ -3,11 +3,17 @@
  */
 package com.github.idelstak.youtubeinfofx.ui;
 
+import java.io.InputStream;
 import java.net.URL;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -16,14 +22,36 @@ import javafx.stage.Stage;
  */
 public class YouTubeInfoFXApp extends Application {
 
+  private static final Logger LOG = Logger.getLogger(YouTubeInfoFXApp.class.getName());
+
   @Override
   public void start(Stage stage) throws Exception {
-    URL resource = YouTubeInfoFXApp.class.getResource("/fxml/api-key-dialog.fxml");
-    FXMLLoader loader = new FXMLLoader(resource);
 
-    Parent root = loader.load();
+    InputStream propsStream = YouTubeInfoFXApp.class.getResourceAsStream("/project/properties/youtube.properties");
 
-    stage.setScene(new Scene(root));
+    LOG.log(Level.INFO, "props{0}", propsStream);
+
+    Properties props = new Properties();
+
+    props.load(propsStream);
+
+    String appName = props.getProperty("application.name");
+    String apiKey = props.getProperty("api.key");
+
+    Scene scene;
+
+    if (appName == null || apiKey == null) {
+      URL resource = YouTubeInfoFXApp.class.getResource("/fxml/api-key-dialog.fxml");
+      FXMLLoader loader = new FXMLLoader(resource);
+
+      Parent root = loader.load();
+      scene = new Scene(root);
+    } else {
+      BorderPane pane = new BorderPane(new Label("YouTubeInfoFX"));
+      scene = new Scene(pane);
+    }
+
+    stage.setScene(scene);
     stage.setTitle("YouTubeInfoFX");
 
     stage.show();
